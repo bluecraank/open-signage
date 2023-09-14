dc54e7ec9eb9ffbe821441f8b984b43d7bac2e51
Nils Fischer <bluecraank@outlook.de>
2023-09-14 20:13:59 +0000

    Custom 403 page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>MIS Manager</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar is-spaced is-dark" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <b>{{ config('app.name') }}</b>
+                </a>
+            </div>
+
+            @auth
+                <div id="navbar-main" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item" href="{{ url()->previous() }}">{{ __('Back to App') }}</a>
+                    </div>
+                </div>
+            @endauth
+        </nav>
+    </div>
+
+    <div id="app">
+        <div class="container">
+            <div class="box main-box">
+                <p class="has-text-centered is-size-4">{{ __('You are not allowed to perform this action') }}</p>
+                <p class="has-text-centered is-size-6">{{ __('If you think this is a mistake, please contact your administrator') }}</p>
+
+
+                <footer>
+                    Contribute on <a href="https://github.com/bluecraank/open-signage">GitHub</a> | Build with <a
+                        href="https://laravel.com">Laravel</a>, <a href="https://bulma.io">Bulma</a> and <i
+                        class="mdi mdi-heart"></i>
+                </footer>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
