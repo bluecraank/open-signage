7087bd137ae8dd0d46789dd2b0e6a6c44e57e7f8
Nils Fischer <bluecraank@outlook.de>
2023-09-14 20:14:35 +0000

    Register device page design

--- resources/views/devices/register.blade.php
+++ resources/views/devices/register.blade.php
@@ -1,6 +1,56 @@
-<form action="{{ route('devices.register') }}" method="POST">
-    @csrf
-    <input type="text" name="secret" id="" placeholder="Secret Key">
-    <button type="submit">Register device</button>
-</form>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>MIS Manager</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar is-spaced is-dark" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <b>{{ config('app.name') }}</b>
+                </a>
+            </div>
+        </nav>
+    </div>
+
+    <div id="app">
+        <div class="container">
+            <div class="box main-box">
+                <form action="{{ route('devices.register') }}" method="POST">
+                    @csrf
+                    <div class="field">
+                        <label class="label">{{ __('Enter secret key') }}:</label>
+                        <input class="input" type="text" name="secret"  placeholder="Secret Key">
+                    </div>
+
+                    <div class="field">
+                        <button class="button is-primary is-fullwidth" type="submit">{{ __('Register device') }}</button>
+                    </div>
+                </form>
+                <footer>
+                    Contribute on <a href="https://github.com/bluecraank/open-signage">GitHub</a> | Build with <a
+                        href="https://laravel.com">Laravel</a>, <a href="https://bulma.io">Bulma</a> and <i
+                        class="mdi mdi-heart"></i>
+                </footer>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
