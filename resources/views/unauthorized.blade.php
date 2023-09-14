28507cc33cdb7e523391f0a6f34d5b39cb2287cc
Nils Fischer <bluecraank@outlook.de>
2023-09-14 20:14:17 +0000

    Unauthorized page

---
+++ resources/views/unauthorized.blade.php
@@ -0,0 +1,2 @@
+<p class="has-text-centered is-size-4">{{ __('You dont have permission to view data') }}</p>
+<p class="has-text-centered is-size-6">{{ __('If you think this is a mistake, please contact your administrator') }}</p>
