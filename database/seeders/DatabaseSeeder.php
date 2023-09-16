083cc7b238fa7cbfe0180b602fdf5284d5f4a627
Nils Fischer <bluecraank@outlook.de>
2023-09-16 00:04:29 +0000

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,79 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Permission::truncate();
-        Role::truncate();
-
-        Permission::updateOrCreate(['name' => 'create devices', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'read devices', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'update devices', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'delete devices', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'create presentations', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'read presentations', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'update presentations', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'delete presentations', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'force reload monitor', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'assign presentations', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'delete slides', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'create users', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'read users', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'update users', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'delete users', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'create groups', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'read groups', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'update groups', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'delete groups', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'assign device to group', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'register devices', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'create schedules', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'read schedules', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'update schedules', 'guard_name' => 'sanctum']);
-        Permission::updateOrCreate(['name' => 'delete schedules', 'guard_name' => 'sanctum']);
-
-        Role::updateOrCreate(['name' => 'Super Administrator', 'guard_name' => 'sanctum'])
-            ->givePermissionTo(Permission::all());
-
-        Role::updateOrCreate(['name' => 'Supporter', 'guard_name' => 'sanctum'])
-            ->givePermissionTo([
-                'read devices',
-                'read presentations',
-                'read groups',
-                'force reload monitor',
-                'register devices'
-            ]);
-
-        Role::updateOrCreate(['name' => 'Manager', 'guard_name' => 'sanctum'])
-            ->givePermissionTo([
-                'update devices',
-                'read devices',
-                'create presentations',
-                'read presentations',
-                'update presentations',
-                'delete presentations',
-                'force reload monitor',
-                'assign presentations',
-                'delete slides',
-                'create groups',
-                'read groups',
-                'update groups',
-                'delete groups',
-                'assign device to group',
-                'create schedules',
-                'read schedules',
-                'update schedules',
-                'delete schedules',
-            ]);
-
-        Role::updateOrCreate(['name' => 'User', 'guard_name' => 'sanctum'])
-            ->givePermissionTo([
-                'read devices',
-                // 'read presentations',
-                // 'read groups'
-            ]);
-
-        $user = User::whereId(1)->first();
-        if($user) {
-            $user->assignRole('Super Administrator');
-        }
+        $this->call([
+            // UserSeeder::class,
+            SettingsSeeder::class,
+            PermissionSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::truncate();
+        Role::truncate();
+
+        Permission::updateOrCreate(['name' => 'create devices', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'read devices', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'update devices', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'delete devices', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'create presentations', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'read presentations', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'update presentations', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'delete presentations', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'force reload monitor', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'assign presentations', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'delete slides', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'create users', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'read users', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'update users', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'delete users', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'create groups', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'read groups', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'update groups', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'delete groups', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'assign device to group', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'register devices', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'create schedules', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'read schedules', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'update schedules', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'delete schedules', 'guard_name' => 'sanctum']);
+        Permission::updateOrCreate(['name' => 'manage settings', 'guard_name' => 'sanctum']);
+
+        Role::updateOrCreate(['name' => 'Super Administrator', 'guard_name' => 'sanctum'])
+            ->givePermissionTo(Permission::all());
+
+        Role::updateOrCreate(['name' => 'Supporter', 'guard_name' => 'sanctum'])
+            ->givePermissionTo([
+                'read devices',
+                'read presentations',
+                'read groups',
+                'force reload monitor',
+                'register devices'
+            ]);
+
+        Role::updateOrCreate(['name' => 'Manager', 'guard_name' => 'sanctum'])
+            ->givePermissionTo([
+                'update devices',
+                'read devices',
+                'create presentations',
+                'read presentations',
+                'update presentations',
+                'delete presentations',
+                'force reload monitor',
+                'assign presentations',
+                'delete slides',
+                'create groups',
+                'read groups',
+                'update groups',
+                'delete groups',
+                'assign device to group',
+                'create schedules',
+                'read schedules',
+                'update schedules',
+                'delete schedules',
+            ]);
+
+        Role::updateOrCreate(['name' => 'User', 'guard_name' => 'sanctum'])
+            ->givePermissionTo([
+                'read devices',
+                // 'read presentations',
+                // 'read groups'
+            ]);
+
+
+        $user = User::whereId(1)->first();
+        if($user) {
+            $user->assignRole('Super Administrator');
+        }
+    }
+}
