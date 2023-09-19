b6096e51ef767d0c7601ab0ab0bd1de1d0322c06
Nils Fischer <bluecraank@outlook.de>
2023-09-19 07:43:28 +0000

    Better error page

--- resources/views/unassigned.blade.php
+++ resources/views/unassigned.blade.php
@@ -1,18 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <meta http-equiv='refresh' content='1'>
+    <title>Error - MIS Manager</title>
+    <meta http-equiv='refresh' content='5'>
 </head>
+
 <body>
     <style>
         body {
-            background-image: url('https://placehold.co/1920x1080?text={{ __('No presentation assigned') }}');
-            background-size: cover;
+            background-color: #efefef;
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            width: 100vw;
+        }
+        .container {
+            display: flex;
+            height: 100%;
+            width: 100%;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+        .text {
+            font-size: 6rem;
+            color: #9c9c9c;
+            font-family: Arial, Helvetica, sans-serif;
         }
     </style>
+    <div class="container">
+        <div class="text">
+            @if ($error == 'no_pres_assigned')
+                {{ __('No presentation assigned') }}
+            @elseif($error == 'no_device')
+                {{ __('Device not found') }}
+            @elseif($error == 'not_registered')
+                {{ __('Device not registered') }}
+            @endif
+        </div>
+    </div>
 </body>
+
 </html>
