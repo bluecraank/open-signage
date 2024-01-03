b426b1b54107db3d5d8c9bcfee045647680d6572
Nils Fischer <bluecraank@outlook.de>
2024-01-03 13:27:48 +0000

    Authentik X-Authentik-Username Key

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use LdapRecord\Laravel\Middleware\WindowsAuthenticate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        WindowsAuthenticate::serverKey('X-Authentik-Username');
     }
 }
