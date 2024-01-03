8a906c5654abbaa3808f9c1164663aff63393a76
Nils Fischer <bluecraank@outlook.de>
2024-01-03 12:49:05 +0000

    Enabling SSO

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \LdapRecord\Laravel\Middleware\WindowsAuthenticate::class,
         ],
 
         'api' => [
