9fcb25b49c83cd84f1c60fc1c60f1f83d6cd4224
Nils Fischer <bluecraank@outlook.de>
2024-01-14 13:08:02 +0000

    Add total count of slides in process message

--- resources/views/livewire/poll-presentation-process.blade.php
+++ resources/views/livewire/poll-presentation-process.blade.php
@@ -2,7 +2,7 @@
     @if ($currentPresentation)
         <span class="processing_info is-flex is-align-content-center">
             <span class="loader"></span>
-            <span class="ml-2 is-">{{ __('Processing') }} {{ $currentPresentation->name }}</span>
+            <span class="ml-2 is-">{{ __('Processing') }} {{ $currentPresentation->name }} ({{ $currentPresentation->slides()->count() }}/{{ $currentPresentation->total_slides }})</span>
         </span>
     @endif
 </div>
