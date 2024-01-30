adf68f3ead909fbfef464254b4e352e84a1cc13c
Nils Fischer <bluecraank@outlook.de>
2024-01-30 12:04:04 +0000

    Fix logging

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -9,7 +9,7 @@ class LogController extends Controller
 {
     public function index()
     {
-        $logs = Log::orderBy('created_at', 'desc')->paginate(10);
+        $logs = Log::orderBy('created_at', 'desc')->get()->take(100);
         return view('logs.index', compact('logs'));
     }
 }
