102051ec79feba2a8108fcb634d759368b062a65
Nils Fischer <bluecraank@outlook.de>
2024-03-13 08:35:05 +0000

    Bump 1.5.6

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'version' => '1.5.5',
+    'version' => '1.5.6',
 
     'placeholder_image' => 'https://placehold.co/600x338?text=Assign\nTemplate',
 
